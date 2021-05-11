8c6c05bf758af3fc1594d88d65cb71deede78de8
Daniel Vitor de Oliveira Santos <danielvitor2000_@hotmail.com>
2021-05-11 14:13:55 -0300

    adicionando um formulario de teste

--- src/main/java/app/Aplicacao.java
+++ src/main/java/app/Aplicacao.java
@@ -1,5 +1,24 @@
 package app;
 
+import static spark.Spark.*;
+
+import service.AnotacoesService;
+
 public class Aplicacao {
-    
+    private static AnotacoesService usuarioService = new AnotacoesService();
+
+    public static void main(String[] args) {
+        port(6789);
+
+        post("/usuario", (request, response) -> usuarioService.addAnotacao(request, response));
+
+        get("/usuario/:id", (request, response) -> usuarioService.getAnotacao(request, response));
+
+        get("/usuario/update/:id", (request, response) -> usuarioService.updateAnotacao(request, response));
+
+        get("/usuario/delete/:id", (request, response) -> usuarioService.removeAnotacao(request, response));
+
+        get("/usuario", (request, response) -> usuarioService.removeAnotacao(request, response));
+        
+    }
 }
