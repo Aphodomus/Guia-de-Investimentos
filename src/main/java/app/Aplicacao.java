84c7950ad95ab5dc49592cd47ea29eff811b41e0
Daniel Vitor de Oliveira Santos <danielvitor2000_@hotmail.com>
2021-05-11 15:12:31 -0300

    Terminando o CRUD, iniciando testes

--- src/main/java/app/Aplicacao.java
+++ src/main/java/app/Aplicacao.java
@@ -10,15 +10,15 @@ public class Aplicacao {
     public static void main(String[] args) {
         port(6789);
 
-        post("/usuario", (request, response) -> usuarioService.addAnotacao(request, response));
+        post("/anotacoes", (request, response) -> usuarioService.addAnotacao(request, response));
 
-        get("/usuario/:id", (request, response) -> usuarioService.getAnotacao(request, response));
+        get("/anotacoes/:id", (request, response) -> usuarioService.getAnotacao(request, response));
 
-        get("/usuario/update/:id", (request, response) -> usuarioService.updateAnotacao(request, response));
+        get("/anotacoes/update/:id", (request, response) -> usuarioService.updateAnotacao(request, response));
 
-        get("/usuario/delete/:id", (request, response) -> usuarioService.removeAnotacao(request, response));
+        get("/anotacoes/delete/:id", (request, response) -> usuarioService.removeAnotacao(request, response));
 
-        get("/usuario", (request, response) -> usuarioService.removeAnotacao(request, response));
+        get("/anotacoes", (request, response) -> usuarioService.getAllAnotacoes(request, response));
         
     }
 }
