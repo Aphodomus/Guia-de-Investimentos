65da216842caddb17133fd0cd36f827359421156
Daniel Vitor de Oliveira Santos <danielvitor2000_@hotmail.com>
2021-05-16 16:27:43 -0300

    Testando

--- src/main/java/app/Aplicacao.java
+++ src/main/java/app/Aplicacao.java
@@ -5,20 +5,20 @@ import static spark.Spark.*;
 import service.AnotacoesService;
 
 public class Aplicacao {
-    private static AnotacoesService usuarioService = new AnotacoesService();
+    private static AnotacoesService anotacoesService = new AnotacoesService();
 
     public static void main(String[] args) {
         port(6789);
 
-        post("/anotacoes", (request, response) -> usuarioService.addAnotacao(request, response));
+        post("/anotacoes", (request, response) -> anotacoesService.addAnotacao(request, response));
 
-        get("/anotacoes/:idanotacoes", (request, response) -> usuarioService.getAnotacao(request, response));
+        get("/anotacoes/:idanotacoes", (request, response) -> anotacoesService.getAnotacao(request, response));
 
-        get("/anotacoes/update/:idanotacoes", (request, response) -> usuarioService.updateAnotacao(request, response));
+        get("/anotacoes/update/:idanotacoes", (request, response) -> anotacoesService.updateAnotacao(request, response));
 
-        get("/anotacoes/delete/:idanotacoes", (request, response) -> usuarioService.removeAnotacao(request, response));
+        get("/anotacoes/delete/:idanotacoes", (request, response) -> anotacoesService.removeAnotacao(request, response));
 
-        get("/anotacoes", (request, response) -> usuarioService.getAllAnotacoes(request, response));
+        get("/anotacoes", (request, response) -> anotacoesService.getAllAnotacoes(request, response));
         
     }
 }
